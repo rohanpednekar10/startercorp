8efc9f90d0030883baae949d8d1cec8b5da0c4bb
ROHAN PEDNEKAR <rohanpednekar10@gmail.com>
2020-06-26 00:09:34 +0530

    may be final

--- users/forms.py
+++ users/forms.py
@@ -16,7 +16,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
